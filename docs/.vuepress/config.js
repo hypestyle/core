673fa56f765b1ef142024546aa26e72bff4c7f3a
devlassev <vestergaardlasse2@gmail.com>
2021-11-16 20:59:30 +0100

    v1 dev

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  title: "HypeStyle CSS",
+  description:
+    "HypeStyle CSS is a collection of CSS utilities for web development.",
+
+  themeConfig: {
+    // search
+    search: true,
+    searchMaxSuggestions: 10,
+    nav: [
+      { text: "Home", link: "/" },
+      { text: "Guide", link: "/guide/" },
+      { text: "Examples", link: "/examples/" },
+      { text: "Github", link: "https://github.com/lassev05/hypestyle" },
+    ],
+
+    sidebar: "auto",
+  },
+};
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+heroImage: /img/icon.png
+actionText: Get Started
+actionLink: /guide/
+
+features:
+  - title: Easy to Use
+    details: Create a beautiful landing page in minutes.
+  - title: Flexible
+    details: Add sections, images and content in any order.
+  - title: Free
+    details: No credit card required, no sign up, no ads.
+
+footer: MIT Licensed | Copyright © 2021
+---
