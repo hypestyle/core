44c141e8b8e00e7e0dcc1069e851eddb05c0dacf
lassev05 <vestergaardlasse2@gmail.com>
2022-06-13 11:17:42 +0200

    Config file added

---
+++ hypestyle.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    outDir: './dist/css',
+
+    colors: {
+        primary: '#326dee ',
+        secondary: '#1ac886',
+        error: '#d32752',
+        info: '#f6c31c ',
+        teal: '#20c997 ',
+    },
+}
