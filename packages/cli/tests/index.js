9d8d1b36d5e353824564d91f28f09a19b7f47718
lassev05 <vestergaardlasse2@gmail.com>
2022-06-24 21:04:12 +0200

    TEST CLI => can build out to css now

--- packages/cli/tests/index.js
+++ packages/cli/tests/index.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
+const path = require('path')
+const chalk = require('chalk')
 
 process.argv.forEach((cmd) => {
     if (cmd === 'build') {
@@ -12,7 +14,7 @@ process.argv.forEach((cmd) => {
             return fs.readFileSync(`./tests/${file}`, 'utf8')
         })
 
-        const cssFile = fs.readFileSync('./tests/index.css', 'utf8')
+        const cssFile = fs.readFileSync('./tests/hypestyle@0.1.9.css', 'utf8')
 
         const classes = contents
             .map((content) => {
@@ -26,12 +28,42 @@ process.argv.forEach((cmd) => {
 
         classes.forEach((className) => {
             if (cssFile.includes(className)) {
-                console.log(`${className} matches with the class with a class!`)
-            } else {
-                console.log(
-                    `No classes match for ${className} in the css file.`
-                )
+                console.log(chalk.green.bold(className))
+
+                const regex = new RegExp(`\\.${className}\\s*{([^}]+)}`, 'g')
+                const matches = cssFile.match(regex)
+                if (matches) {
+                    matches.forEach((match) => {
+                        console.log(chalk.yellow(match))
+
+                        const output = path.join('./tests', `outPut.css`)
+
+                        fs.appendFileSync(output, match)
+                    })
+                }
             }
+        })
+    } else if (cmd === '--watch') {
+        console.log(chalk.hex('#e55d3b').bold('Watching for changes...'))
+
+        const watcher = require('chokidar').watch('./tests', {
+            ignored: /(^|[\/\\])\../,
+            persistent: true,
+        })
+
+        watcher.on('change', (path) => {
+            console.log(`File ${path} has been changed`)
+
+            // run script
+            require('child_process').exec(
+                `pnpm run test build --watch`,
+                (err, stdout, stderr) => {
+                    if (err) {
+                        console.log(err)
+                    }
+                    console.log(stdout)
+                }
+            )
         })
     }
 })
